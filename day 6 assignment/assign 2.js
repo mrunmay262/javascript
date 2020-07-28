58a65852c5020287547f3d6aa93951702641f3fb
mrunmay262 <68142490+mrunmay262@users.noreply.github.com>
2020-07-27 22:35:27 -0700

    Add files via upload

---
+++ day 6 assignment/assign 2.js
@@ -0,0 +1,13 @@
+javascript code:
+
+
+<script type="text/javascript">
+	function printTable(){
+		var num;
+		num=Number(document.getElementById('txtNumber').value);
+		for(var i=1; i<=10; i++){
+			var pTag= document.getElementById('pPrint');
+			pTag.innerHTML += (num*i) + "<br/>" 
+		}
+	}
+</script>
---
+++ day 6 assignment/assignment 1.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+  var c = ["blue", "black", "yellow", "red"];
+
+  function changeColor() {
+    setTimeout(function loop() {
+      document.bgColor = c.shift();
+      if (c.length) {
+        setTimeout(loop, 5000);
+      }
+    }, 5000);
+  }
+
+  function start() {
+    setInterval(changeColor(), 20000);
+  }
+</script>
+
+<body onload="start()">
+</body>
